72dcc204df7e0c652d3c1eeb7bad8d3493303a97
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-01-28 00:14:31 +0530

    Update background removal.py

--- background removal.py
+++ background removal.py
@@ -1,8 +1,20 @@
 # import cv2 to capture videofeed
 import cv2
 
+import numpy as np
+
 # attach camera indexed as 0
 camera = cv2.VideoCapture(0)
+
+# setting framewidth and frameheight as 640 X 480
+camera.set(3 , 640)
+camera.set(4 , 480)
+
+# loading the mountain image
+mountain = cv2.imread('mount everest.jpg')
+
+# resizing the mountain image as 640 X 480
+
 
 while True:
 
@@ -14,6 +26,21 @@ while True:
 
         # flip it
         frame = cv2.flip(frame , 1)
+
+        # converting the image to RGB for easy processing
+        frame_rgb = cv2.cvtColor(frame , cv2.COLOR_BGR2RGB)
+
+        # creating thresholds
+        lower_bound = np.array([])
+        upper_bound = np.array([])
+
+        # thresholding image
+
+        # inverting the mask
+
+        # bitwise and operation to extract foreground / person
+
+        # final image
 
         # show it
         cv2.imshow('frame' , frame)
