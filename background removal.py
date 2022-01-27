a563efbfae0fa79bdff8c76d0dbe3de0ce4be649
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-01-27 23:34:53 +0530

    Add files via upload

---
+++ background removal.py
@@ -0,0 +1,28 @@
+# import cv2 to capture videofeed
+import cv2
+
+# attach camera indexed as 0
+camera = cv2.VideoCapture(0)
+
+while True:
+
+    # read a frame from the attached camera
+    status , frame = camera.read()
+
+    # if we got the frame successfully
+    if status:
+
+        # flip it
+        frame = cv2.flip(frame , 1)
+
+        # show it
+        cv2.imshow('frame' , frame)
+
+        # wait of 1ms before displaying another frame
+        code = cv2.waitKey(1)
+        if code  ==  32:
+            break
+
+# release the camera and close all opened windows
+camera.release()
+cv2.destroyAllWindows()
